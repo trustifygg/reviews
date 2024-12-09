66b85fe5e6b383eb77e17c18323362d24adb93dc
Lukass <fraimerdelacruzjaziel@gmail.com>
2024-12-09 12:42:38 -0400

    Implement quick setup command

---
+++ src/commands/chatInputCommands/core/quicksetup.ts
@@ -0,0 +1,76 @@
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ChannelType,
+	EmbedBuilder,
+	SlashCommandBuilder,
+	type ChatInputCommandInteraction,
+} from 'discord.js';
+
+import { ApplyCommandOption } from '#structure/Command';
+import { ChatInputCommand } from '#structure/ChatInputCommand';
+import { guildModel } from '#model/guild';
+
+@ApplyCommandOption(new SlashCommandBuilder().setName('quicksetup').setDescription('...'), { allowDM: false })
+export class UserCommand extends ChatInputCommand {
+	protected override async runTask(interaction: ChatInputCommandInteraction<'cached'>) {
+		const initialEmbed = new EmbedBuilder()
+			.setTitle('Quick Setup')
+			.setColor('Blurple')
+			.setDescription(
+				`To get started, click the button below to begin the automatic setup.\n*This usually takes a few minutes*\n\nTo further customize the bot, head over to your server's [dashboard](${process.env.WEBSITE_URL}/dashboard/${interaction.guildId}).`
+			)
+			.setFooter({ text: 'Thanks for choosing Reviews!' });
+
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+			new ButtonBuilder().setCustomId('quicksetup').setLabel('Begin Quick Setup').setStyle(ButtonStyle.Primary),
+			new ButtonBuilder()
+				.setURL(`${process.env.WEBSITE_URL}/dashboard/${interaction.guildId}`)
+				.setEmoji('🌐')
+				.setLabel('Go to the Dashboard')
+				.setStyle(ButtonStyle.Link)
+		);
+
+		const res = await interaction.reply({ embeds: [initialEmbed], components: [row], ephemeral: true });
+
+		const collector = res.createMessageComponentCollector({ time: 60000 });
+
+		collector.on('collect', async (i) => {
+			if (i.customId !== 'quicksetup') return;
+
+			const guildData = await guildModel.findOne({ guildId: interaction.guildId });
+			if (!guildData) return await guildModel.create({ guildId: interaction.guildId });
+
+			if (guildData.channel) return interaction.reply('This server has already been setup.');
+
+			const reviewCategory = await interaction.guild.channels.create({
+				name: 'Reviews',
+				type: ChannelType.GuildCategory,
+			});
+
+			const channel = await interaction.guild.channels.create({
+				name: 'reviews',
+				type: ChannelType.GuildText,
+				parent: reviewCategory.id,
+			});
+
+			guildData.channel = channel.id;
+
+			const logsChannel = await interaction.guild.channels.create({
+				name: 'reviews-logs',
+				type: ChannelType.GuildText,
+				parent: reviewCategory.id,
+			});
+
+			guildData.logsChannel = logsChannel.id;
+
+			await guildData.save();
+
+			await i.reply({
+				content: 'Setup complete! You can now start using the bot by typing `/review`.',
+				ephemeral: true,
+			});
+		});
+	}
+}
--- src/events/client/guildCreate.ts
+++ src/events/client/guildCreate.ts
@@ -1,6 +1,8 @@
 import type { ClientEvents } from '#types/events';
 
 import { guildModel } from '#model/guild';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } from 'discord.js';
+import { Constants } from '#util/constants';
 
 const guildCreateEvent: ClientEvents['GuildCreate'] = async (guild) => {
 	const data = await guildModel.findOne({ guildId: guild.id });
@@ -10,6 +12,33 @@ const guildCreateEvent: ClientEvents['GuildCreate'] = async (guild) => {
 		name: guild.name,
 		iconURL: guild.iconURL(),
 	});
+
+	const joinEmbed = new EmbedBuilder()
+		.setAuthor({ name: '👋 Thanks for choosing Reviews!', iconURL: guild.client.user.displayAvatarURL() })
+		.setColor('Blurple')
+		.setDescription(
+			`To get started use the \`/quicksetup\` to begin the automatic setup or visit the [dashboard](${process.env.WEBSITE_URL}/dashboard/${guild.id}) to further customize the me!`
+		)
+		.addFields({
+			name: 'Help',
+			value:
+				'To view my commands, use the `/help` command. Additionally, if you encounter any issues or need help, feel free to join our [support server](${Constants.supportInviteLink}).',
+		});
+
+	const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+		new ButtonBuilder()
+			.setURL(`${process.env.WEBSITE_URL}/dashboard/${guild.id}`)
+			.setEmoji('🌐')
+			.setLabel('Go to the Dashboard')
+			.setStyle(ButtonStyle.Link),
+		new ButtonBuilder()
+			.setStyle(ButtonStyle.Link)
+			.setEmoji('🏠')
+			.setLabel('Support Server')
+			.setURL(Constants.supportInviteLink)
+	);
+
+	guild.systemChannel?.send({ embeds: [joinEmbed], components: [row] });
 };
 
 export default guildCreateEvent;
